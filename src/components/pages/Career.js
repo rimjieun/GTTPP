9db11c719a02a8a8a66f3522e37633c287f23dfc
Jieun Rim <rim.jieun@gmail.com>
2017-10-28 13:20:42 -0400

    initial files

---
+++ src/components/pages/Career.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Career = props => (
+  <div>
+    This is the Career Development page.
+  </div>
+);
+
+export default Career;
---
+++ src/components/pages/Events.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Events = props => (
+  <div>
+    This is the Upcoming Events page.
+  </div>
+);
+
+export default Events;
