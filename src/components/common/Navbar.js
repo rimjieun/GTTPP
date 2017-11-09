c61f448e8d7a6819ade74ec3fcea79f38e53f75e
Jieun Rim <rim.jieun@gmail.com>
2017-11-09 13:46:27 -0500

    saving changes

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -10,13 +10,21 @@ const Navbar = props => (
         </Link>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li><Link to="/">HOME</Link></li>
-          <li><Link to="/alumni">ALUMNI/GET INVOLVED</Link></li>
-          <li><Link to="#">DEVELOPMENT</Link></li>
-          <li><Link to="/stories">OUR STORIES</Link></li>
+          <li><Link to="/alumni-get-involved">ALUMNI/GET INVOLVED</Link></li>
+          <li><Link to="#!" className="dropdown-button" data-activates="development">DEVELOPMENT<i className="material-icons right">arrow_drop_down</i></Link></li>
+          <li><Link to="/our-stories">OUR STORIES</Link></li>
           <li><Link to="/events">EVENTS</Link></li>
         </ul>
       </div>
     </nav>
+    <ul id="development" className="dropdown-content">
+      <li><Link to="/career-development">CAREER DEVELOPMENT</Link></li>
+      <li><Link to="/leadership-development">LEADERSHIP DEVELOPMENT</Link></li>
+      <li><Link to="/personal-development">PERSONAL DEVELOPMENT</Link></li>
+      <li className="divider"></li>
+      <li><Link to="/community-outreach">COMMUNITY OUTREACH</Link></li>
+      <li><Link to="/sports-nutrition">SPORTS NUTRITION</Link></li>
+    </ul>
   </div>
 );
 
