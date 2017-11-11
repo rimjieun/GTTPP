d0f736c5fe46a201ef04c364ebf1b48ef7454335
Ryan Rodwell <ryanrodwell@gmail.com>
2017-11-10 23:44:39 -0500

    adding twitter feed

--- src/components/subcomponents/Home/TwitterFeed.js
+++ src/components/subcomponents/Home/TwitterFeed.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const TwitterFeed = props => (
     <div>
-        <h2 className="header">Parallax</h2>
-        <p className="grey-text text-darken-3 lighten-3">Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling.</p>
+        <div className="mr-space" id="mr-space_ramblinwreck_com-georgiatechfb-media_wall" data-space-id="georgia-tech/ramblinwreck_com-georgiatechfb-media_wall" style="min-height: 500px;"></div>
+        <script>!function(a,b,c,d,e,f,g,h,i,j,k){h = a[d] = a[d] || {},h.ui=h.ui||[],i=a[e]=a[e]||{},i[f]||(j=b.getElementsByTagName(c)[0],k=b.createElement(c),k.src="//platform.massrelevance.com/js/massrel.js",j.parentNode.insertBefore(k,j),i[f]=function(){h.ui.push([].slice.call(arguments))}),i[f]("load",{el: b.getElementById(g)})}(window,document,"script","massrel","spredfast","exp","mr-space_ramblinwreck_com-georgiatechfb-media_wall");</script>
     </div>
 );
 
