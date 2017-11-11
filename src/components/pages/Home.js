399d35f087a2e56d7410bd0f2ca0fcc216c39993
Ryan Rodwell <ryanrodwell@gmail.com>
2017-11-10 23:16:21 -0500

    adding jquery easing

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -22,7 +22,7 @@ const Home = props => (
 
     <div className="section white">
       <div className="row container">
-        <h2 className="header">Parallax2</h2>
+        <h2 className="header">Parallax</h2>
         <p className="grey-text text-darken-3 lighten-3">Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling.</p>
       </div>
     </div>
