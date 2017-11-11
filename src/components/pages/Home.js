2f671987cbcedef26adf188f8a5cdaa99f7c2e5b
Ryan Rodwell <ryanrodwell@gmail.com>
2017-11-10 22:49:33 -0500

    updating home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,15 +2,14 @@ import React from 'react';
 
 const Home = props => (
   <div className="main">
-    <div className="carousel carousel-slider">
-        <a className="carousel-item" href="#one!"><img src="https://lorempixel.com/800/400/food/1"/></a>
-        <a className="carousel-item" href="#two!"><img src="https://lorempixel.com/800/400/food/2"/></a>
-        <a className="carousel-item" href="#three!"><img src="https://lorempixel.com/800/400/food/3"/></a>
-        <a className="carousel-item" href="#four!"><img src="https://lorempixel.com/800/400/food/4"/></a>
-    </div>
     <div className="parallax-container">
       <div className="parallax">
-        <img src="./assets/images/jwob/JWOB6.JPG" />
+        <div className="carousel carousel-slider">
+          <a className="carousel-item" href="#one!"><img src="https://lorempixel.com/800/400/food/1" /></a>
+          <a className="carousel-item" href="#two!"><img src="https://lorempixel.com/800/400/food/2" /></a>
+          <a className="carousel-item" href="#three!"><img src="https://lorempixel.com/800/400/food/3" /></a>
+          <a className="carousel-item" href="#four!"><img src="https://lorempixel.com/800/400/food/4" /></a>
+        </div>
       </div>
     </div>
 
