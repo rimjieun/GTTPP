3280ae34ded686ef6c9c48a9a8fabfed82a2b32b
Jieun Rim <rim.jieun@gmail.com>
2017-11-12 18:00:38 -0500

    saving changes

---
+++ src/components/subcomponents/Alumni/Banner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Banner = props => (
+  <div className='banner banner-alumni'>
+    <div className='layer'></div>
+    <div className='banner-text'>
+      <div className='page-container'>
+        <h1 className='banner-alumni'>Athletes Helping Athletes&mdash;</h1>
+        <h3 className='banner-alumni'>A Family for Life</h3>
+        <h6 className='banner-alumni'>We share the common bond of excelling in both our sport and classroom. We are not merely atheletes, but student-athletes. Earning a Letter from the Georgia Institute of Technology is an honor not effortlessly achieved.</h6>
+      </div>
+    </div>
+  </div>
+);
+
+export default Banner;
--- src/components/subcomponents/Alumni/LWC.js
+++ src/components/subcomponents/Alumni/LWC.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 const LWC = props => (
-  <div className='custom-container'>
+  <div className='page-container alumni-lwc'>
     <h1>GT Letterwinners Club</h1>
-    <p>The Georgia Tech Letterwinners Club is committed to serving GT student-athletes after graduation. The club hosts several events throughout the year as opportunities to connect with teammates and peers, suppor current student-athletes, and get involved with the GT Athletic Association.</p>
+    <div className='flex-row content-spread'>
+      <p>The Georgia Tech Letterwinners Club is committed to serving GT student-athletes after graduation. The club hosts several events throughout the year as opportunities to connect with teammates and peers, support current student-athletes, and get involved with the GT Athletic Association.</p>
+      <img className='alumni-lwc' src='./assets/images/community-service/community-service-5.JPG' />
+    </div>
   </div>
 );
 
