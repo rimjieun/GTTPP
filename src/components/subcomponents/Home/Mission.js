afb52b75088be1a2754677dbc273354e3255c61e
Ryan Rodwell <ryanrodwell@gmail.com>
2017-11-12 21:04:26 -0500

    updating

--- src/components/subcomponents/Home/Mission.js
+++ src/components/subcomponents/Home/Mission.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Mission = props => (
-    <div>
+    <div className='page-contaier'>
         <h2 className="header">Mission</h2>
         <p className="grey-text text-darken-3 lighten-3">Georgia Tech Athletics inspires and empowers student-athletes to be champions in academics, competition, and life.</p>
     </div>
