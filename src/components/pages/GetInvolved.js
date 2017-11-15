b685b644a138cf63f667fb70c4c321167b2d3cf1
Ryan Rodwell <ryanrodwell@gmail.com>
2017-11-15 12:58:10 -0500

    adding staff contact section to get involved

--- src/components/pages/GetInvolved.js
+++ src/components/pages/GetInvolved.js
@@ -4,6 +4,9 @@ import { GetInvolvedForm } from "./../subcomponents/Forms";
 const GetInvolved = props => (
   <div>
     <GetInvolvedForm/>
+    <section className='lightgray-bg'>
+      <h1 className="header center">STAFF CONTACT</h1>
+    </section>
   </div>
 );
 
