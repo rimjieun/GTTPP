a2433cc023b7d2c3dd738e74b29f8d1bdc0a2a21
Ryan Rodwell <ryanrodwell@gmail.com>
2017-11-21 13:44:51 -0500

    testing click on leanr more button

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -8,14 +8,14 @@ $(document).ready(function () {
     
     var carouselInterval = setInterval(nextSlide, 6000);
     
-    function pageScroll(pixels){
+    var pageScroll = function(pixels){
       window.scrollTo(0, pixels);
     }
 
-    $('.learn-more').on('click', function() {
-      console.log("clicked");
-      pageScroll(650); 
-    });
+    // $('.learn-more').on('click', function() {
+    //   console.log("clicked");
+    //   pageScroll(650); 
+    // });
 
 
 
