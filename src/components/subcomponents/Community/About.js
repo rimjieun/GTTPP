57ba6806e7519de2ed71880b74ff5722c193cf9b
Ryan Rodwell <ryanrodwell@gmail.com>
2017-12-14 19:21:18 -0500

    adding community service section

---
+++ src/components/subcomponents/Community/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ColLayout } from './../../common';
+
+const styles = {
+    text: {
+        fontSize: '20px',
+        lineHeight: '50px'
+    }
+};
+
+const About = props => (
+    <ColLayout title=''>
+        <p className="header center">Involvement and participation with the community is a vital component of the Total Person Program. Each year, student-athletes get involved with multiple major causes and events to promote awareness, show compassion, and give back to the community that supports them. These events are spearheaded by the Student-Athlete Advisory Board with the intent to provide mentorship to young children in each project they take on.</p>
+        <div>List/show pictures of main service projects.</div>
+    </ColLayout>
+);
+
+export default About;
---
+++ src/components/subcomponents/Community/CommunityService.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ColLayout } from './../../common';
+
+const styles = {
+    text: {
+        fontSize: '20px',
+        lineHeight: '50px'
+    }
+};
+
+const OurStories = props => (
+    <ColLayout title='OUR STORIES'>
+        <p className="header center">Our Stories</p>
+    </ColLayout>
+);
+
+export default OurStories;
