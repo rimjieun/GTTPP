b537d5ae8fd57fe06fe113f84bc50c409ce89e7e
Ryan Rodwell <ryanrodwell@gmail.com>
2017-12-15 12:47:46 -0500

    removing nav dropdown

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -12,21 +12,18 @@ const Navbar = props => (
           <ul id="nav-mobile" className="right hide-on-med-and-down">
             <li><Link to="/">HOME</Link></li>
             <li><Link to="/alumni">ALUMNI</Link></li>
-            <li><Link to="#!" className="dropdown-button" data-activates="development">DEVELOPMENT<i className="material-icons right">arrow_drop_down</i></Link></li>
+            <li><Link to="/career-development">CAREER DEVELOPMENT</Link></li>
+            <li><Link to="/leadership-development">LEADERSHIP DEVELOPMENT</Link></li>
+            <li><Link to="/personal-development">PERSONAL DEVELOPMENT</Link></li>
+            <li className="divider"></li>
+            <li><Link to="/community-outreach">COMMUNITY OUTREACH</Link></li>
+            <li><Link to="/sports-nutrition">SPORTS NUTRITION</Link></li>
             <li><Link to="/our-stories">OUR STORIES</Link></li>
             <li><Link to="/events">EVENTS</Link></li>
             <li><Link to="/get-involved">GET INVOLVED</Link></li>
           </ul>
         </div>
       </nav>
-      <ul id="development" className="dropdown-content">
-        <li><Link to="/career-development">CAREER DEVELOPMENT</Link></li>
-        <li><Link to="/leadership-development">LEADERSHIP DEVELOPMENT</Link></li>
-        <li><Link to="/personal-development">PERSONAL DEVELOPMENT</Link></li>
-        <li className="divider"></li>
-        <li><Link to="/community-outreach">COMMUNITY OUTREACH</Link></li>
-        <li><Link to="/sports-nutrition">SPORTS NUTRITION</Link></li>
-      </ul>
     </div>
   </header>
 );
