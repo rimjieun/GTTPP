05ca41b340f1e29a5ac60309aaf4771f10f143d0
Ryan Rodwell <ryanrodwell@gmail.com>
2018-01-15 21:12:54 -0500

    navbar dropdown fix, adding new pages

--- src/components/subcomponents/Career/GeneralInfo.js
+++ src/components/subcomponents/Career/GeneralInfo.js
@@ -2,18 +2,27 @@ import React from 'react';
 import { ColLayout, TextLeftImgRight, TextRightImgLeft } from './../../common';
 
 const GeneralInfo = props => (
+  <div>
   <ColLayout title='OUR PROGRAM'>
     <p>Georgia Tech's Total Person Program focuses largely on professional development and career programming to prepare student-athletes for their future. This program, called the <span className='italic'>Market Ready Series</span>, strives to provide resources to each student-athlete's specific needs to make them ready for the job market during and after college.</p>
+    </ColLayout>
+  <section className='white-bg black-text'>
     <h5>Market Ready</h5>
     <p>The <span className='italic'>Market Ready Series</span> combines career preparation programs, like business card creation, resume building and critique, and LinkedIn lessons. These opportunities culminate towards our annual fall GT Athletics Career Fair, where student-athletes have the ability to network, interact, and connect with high profile companies interested in hiring studnet-athletes due to their unique transferrable skills.</p>
+
     <p>Additionally, each student-athlete enters a <span className='bold'>Market Ready Pipeline</span> based on their intended industry of employment. With a cohort of peers, each student will select specific programming based on their needs to prepare them with the experience and skills necessary to land a job after graduation.</p>
+  </section>
+  <section className='black-bg black-text'>
     <h5>The Goal</h5>
     <p>With our unique and intentional programming, the <span className='bold'>Total Person Program</span> equips each student-athlete to be successful and prepared for the job market after playing sports.</p>
     <p>As Director of Athletics, Todd Stansbury states, "Our differentiator is the success of our student-athletes five to ten years after graduation." With this as our primary focus, our resources are built around making each individual successful for the long term.</p>
+  </section>
+  <section className='gold-bg black-text'>
     <h5>Our Resources</h5>
     <p>Each student-athlete wil lmeet one-on-one with a staff member to discuss career goals and needs. Additionally, every individual will complete a career inventory and assessment to understand their skills and ensure they are in the major that suits them best.</p>
     <p>To support our programs and the Market Ready Pipelines, we look for partnerships with alumni, organizations, and corporations to offer experience, mentoring and jobs.</p>
-  </ColLayout>
+  </section>
+  </div>
 );
 
 export default GeneralInfo;
