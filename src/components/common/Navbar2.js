ef521f0907eb0bb6b0e4119af33743ceb79d0a4d
Ryan Rodwell <ryanrodwell@gmail.com>
2018-01-29 15:20:27 -0500

    removing demo button

--- src/components/common/Navbar2.js
+++ src/components/common/Navbar2.js
@@ -58,25 +58,6 @@ class Navbar2 extends Component {
                     <NavItem href="/our-stories">OUR STORIES</NavItem>
                     <NavItem href="/get-involved">GET INVOLVED</NavItem>
 
-                    <SideNav
-                        trigger={<Button>SIDE NAV DEMO</Button>}
-                        options={{ closeOnClick: true }}
-                    >
-                        <SideNavItem userView
-                            user={{
-                                background: 'img/office.jpg',
-                                image: 'img/yuna.jpg',
-                                name: 'John Doe',
-                                email: 'jdandturk@gmail.com'
-                            }}
-                        />
-                        <SideNavItem href='#!icon' icon='cloud'>First Link With Icon</SideNavItem>
-                        <SideNavItem href='#!second'>Second Link</SideNavItem>
-                        <SideNavItem divider />
-                        <SideNavItem subheader>Subheader</SideNavItem>
-                        <SideNavItem waves href='#!third'>Third Link With Waves</SideNavItem>
-                    </SideNav>
-
                 </Navbar>
 
             </div>
