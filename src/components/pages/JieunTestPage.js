fd667b7f39c1102e814d05e688eaec315f5829dc
Jieun Rim <rim.jieun@gmail.com>
2018-02-28 21:48:47 -0500

    created PictureRow component

--- src/components/pages/JieunTestPage.js
+++ src/components/pages/JieunTestPage.js
@@ -1,19 +1,46 @@
 import React, { Component } from 'react';
+import { Row } from 'react-materialize';
+import PictureRow from '../../newComponents/PictureRow';
 
 class JieunTestPage extends Component {
   constructor() {
     super();
-
+    this.state = {
+      staff: [
+        {
+          name: 'Akin Joeleen',
+          imgPath: './assets/images/staff/Akin_Joeleen.jpg'
+        },
+        {
+          name: 'Leah Thomas',
+          imgPath: './assets/images/staff/Leah_Thomas.jpg'
+        },
+        {
+          name: 'Sanford Lucius',
+          imgPath: './assets/images/staff/sanford_lucius.jpg'
+        },
+        {
+          name: 'Tremblay Maureen',
+          imgPath: './assets/images/staff/Tremblay_Maureen.jpg'
+        },
+        {
+          name: 'Sanford Lucius',
+          imgPath: './assets/images/staff/sanford_lucius.jpg'
+        }
+      ]
+    }
   }
 
-  componentDidMount() {
-
-  }
 
   render () {
     return (
       <div>
         <h1>Hi Jieun!</h1>
+        <div style={{display: 'flex'}}>
+          {
+            this.state.staff.map(member => <PictureRow imgPath={member.imgPath} memberName={member.name} />)
+          }
+        </div>
       </div>
     )}
   };
